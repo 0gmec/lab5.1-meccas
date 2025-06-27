191e3fb66aa11ad5663cb5903ed3af4084b1579e
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-06-27 12:14:36 -0400

    added products and remove

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const productNameInput = document.getElementById('product-name');
-const productPriceInput = document.getElementById('product-price');
+const productName = document.getElementById('product-name');
+const productPrice = document.getElementById('product-price');
 const addProductButton = document.getElementById('add-product');
 const cart = document.getElementById('cart');
 const totalPriceSpan = document.getElementById('total-price');
-const removeProductButton = document.getElementById(`remove-product`);
- 
+
 let totalPrice = 0;
  
 // Function to update the total price
@@ -21,13 +20,49 @@ function removeItem(event) {
   item.remove();
 }
 
-function createProduct(productName, productPrice) {
-    return {
-        productName: nameInput,
-        productPrice: price
-        
-    };
+//add product button function
+addProductButton.addEventListener('click', function() {
+    
+})
+
+
+// Function for adding products and testing 
+let productList = []
+
+function addProductToList(name, price) {
+const newProduct = {
+    name: name,
+    price: parseFloat(price)
+
+    
+};
+productList.push(newProduct);
+renderProductToList(newProduct);
+updateTotalPrice(newProduct.price);
+
 }
-const product = createProduct("Shoes", 65);
-console.log(product.productName);
-console.log(product.productPrice);
+
+//Totaling cost, removebutton child 
+function renderProductToList(product) {
+    
+const listItem = document.createElement('li');
+listItem.textContent = `${product.name} - $${product.price.toFixed(2)}`;
+listItem.dataset.price = product.price;
+
+const removeBtn = document.createElement('button');
+removeBtn.textContent = 'Remove'
+removeBtn.addEventListener('click',removeItem);
+
+listItem.appendChild(removeBtn);
+cart.appendChild(listItem);
+    
+}
+
+addProductToList("Cat food", 9.00)
+addProductToList("Icecream", 6.38);
+addProductToList("soap", 3.00);
+addProductToList("cookies", 5.99);
+addProductToList("chips", 1.99);
+addProductToList("napkins", 2.50)
+
+console.log(productList);
